264f9298fd8c83b3d5185a1b4fc77f373cb85be6
Ikameloth <matteo.camilo.k@gmail.com>
2022-04-12 21:27:30 -0400

    404 not found view added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
-import type { NextPage } from 'next'
+import { Typography } from "@mui/material";
+import type { NextPage } from "next";
+import { ShopLayout } from "../components/layout";
 
 const HomePage: NextPage = () => {
   return (
-    <h1>Teslo shop</h1>
-  )
-}
+    <ShopLayout
+      title={"Teslo-Shop"}
+      pageDescription={"Best store ever ziono raza"}
+    >
+      <Typography variant="h1" component="h1">
+        Store
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All products
+      </Typography>
+    </ShopLayout>
+  );
+};
 
-export default HomePage
+export default HomePage;
